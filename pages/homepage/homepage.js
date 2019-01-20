617701df64ca7b11b76f329c43733f761fdac1bc
Moon <justwe77@163.com>
2019-01-20 20:54:29 +0800

    first commit

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,96 @@
+const app = getApp()
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    background: [
+      'https://a1.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0108%2Fr311644_1296x729_16%2D9.jpg&w=920&h=518&scale=crop&cquality=80&location=origin',
+      'https://www.northamptonma.gov/ImageRepository/Document?documentID=9915',
+      'https://www.bracknell-forest.gov.uk/sites/default/files/tennis-header-image.jpg'
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
+  },
+  changeProperty: function(e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  onLoad: function() {
+    console.log(11111)
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          console.log(res)
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+          console.log(app.globalData.userInfo)
+        }
+      })
+    }
+    console.log(22222)
+
+    // 查看是否授权
+    wx.getSetting({
+      success: function(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+
+  bindGetUserInfo: function(e) {
+    console.log(e.detail.userInfo)
+  }
+
+})
